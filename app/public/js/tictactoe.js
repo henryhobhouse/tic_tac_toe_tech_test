258b52be5303adb06ace581b37564554c10417d2
Henry Hobhouse <henryhobhouse@gmail.com>
2017-06-16 15:57:06 +0100

    winner can be declared when correct row combination is done

--- app/public/js/tictactoe.js
+++ app/public/js/tictactoe.js
@@ -7,21 +7,20 @@ function TicTacToe () {
 }
 
 TicTacToe.prototype.board = function() {
-  var i = '';
-  for (i = 0; i < 9; i++) {
-    var sqr = new this.square();
-    this.squares.push(sqr);
+  for (let i = 0; i < 9; i++) {
+    // var sqr = new this.square();
+    this.squares.push(null);
   }
 };
 
 // Controls player turn
-TicTacToe.prototype.onclick = function() {
+TicTacToe.prototype.on_click = function() {
 
 };
 
 // Changes player go
 TicTacToe.prototype.swap_player = function() {
-  this.xIsNext = this.xIsNext == true ? false : true;
+  this.xIsNext = this.xIsNext === true ? false : true;
 };
 
 // manipulates state of square
@@ -31,11 +30,25 @@ TicTacToe.prototype.change_state = function() {
 
 // checks if winning combination of turns has occured
 TicTacToe.prototype.check_winner = function() {
-
-};
-
-TicTacToe.prototype.square = function() {
-  this.state = null;
+  var win_comb = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ];
+  for (let i = 0; i < win_comb.length; i++) {
+    var [a, b, c] = win_comb[i];
+    if(this.squares[a] === this.squares[b] &&
+       this.squares[c] === this.squares[b] &&
+       this.squares[a] !== null) {
+      return this.squares[a];
+    }
+  }
+  return null;
 };
 
 module.exports = TicTacToe;
--- spec/jasmine/tictactoe_spec.js
+++ spec/jasmine/tictactoe_spec.js
@@ -2,25 +2,53 @@ describe("TicTacToe", function() {
   var TicTacToe = require('../../app/public/js/tictactoe.js');
   var TicTacToe;
 
-  beforeEach(function() {
-    ttt = new TicTacToe();
-  });
+  describe('Post initialization', function() {
+    beforeEach(function() {
+      ttt = new TicTacToe();
+    });
 
-  it('It initializes on xIsNext as ture', function () {
-    expect(ttt.xIsNext).toEqual(true);
-  });
+    it('It initializes on xIsNext as ture', function () {
+      expect(ttt.xIsNext).toBeTruthy();
+    });
 
-  it('It creates a board with 9 square objects', function() {
-    expect(ttt.squares.length).toEqual(9);
-    // expect(ttt.squares[0])toEqual(ttt.square);
-  });
+    it('It creates a array with 9 values', function() {
+      expect(ttt.squares.length).toEqual(9);
+    });
 
-  it('Each square has a instance vairbable, state, set as null', function() {
-    expect(ttt.squares[0].state).toEqual(null);
-  });
+    it('Each array value is set to null on initialization', function() {
+      const ary = new Array(9).fill(null);
+      expect(ttt.squares).toEqual(ary);
+    });
 
-  it('swap players changes xIsTrue', function() {
-    ttt.swap_player();
-    expect(ttt.xIsNext).toEqual(false);
+    it('swap players changes xIsTrue', function() {
+      ttt.swap_player();
+      expect(ttt.xIsNext).toBeFalsy();
+    });
+
+    it('returns winner horizontal line', function() {
+      ttt.squares[0] = ttt.squares[1] = ttt.squares[2] = 'X'
+      expect(ttt.check_winner()).toEqual('X')
+    });
+
+    it('returns winner vertical line', function() {
+      ttt.squares[2] = ttt.squares[5] = ttt.squares[8] = 'X'
+      expect(ttt.check_winner()).toEqual('X')
+    });
+
+    it('returns winner diaganal line', function() {
+      ttt.squares[6] = ttt.squares[4] = ttt.squares[2] = 'X'
+      expect(ttt.check_winner()).toEqual('X')
+    });
+
+    it('returns no winner upon no rows', function() {
+      ttt.squares[6] = ttt.squares[3] = ttt.squares[2] = 'X'
+      expect(ttt.check_winner()).toBeNull();
+    });
+
+    it('returns no winner on mixed rows', function() {
+      ttt.squares[0] = ttt.squares[1] = 'X'
+      ttt.squares[0] = 'O'
+      expect(ttt.check_winner()).toBeNull();
+    });
   });
 });
